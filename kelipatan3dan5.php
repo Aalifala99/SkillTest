455f751a5a1c1548ee865dec274ec164ad32974f
Danial <79654560+Aalifala99@users.noreply.github.com>
2022-10-07 14:53:23 +0700

    Update kelipatan3dan5.php
    Update angka aja si

--- kelipatan3dan5.php
+++ kelipatan3dan5.php
@@ -1,6 +1,6 @@
 <?php
-//menampilkan angka 1-100 
-for ($i = 1; $i <= 100; $i++) {
+//menampilkan angka 1-15 
+for ($i = 1; $i <= 15; $i++) {
   if ($i % 5 == 0 && $i % 3 == 0) {
     echo "fizzbuzz\n";
   } elseif ($i % 3 == 0) {
